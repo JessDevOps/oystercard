ee513005e0c1cddf8e80471c80a19926136778f7
Jessica <Jessica_doyle@outlook.com>
2021-07-26 15:08:01 +0100

    Challenge 2 complete: add money to card

---
+++ lib/oystercard.rb
@@ -0,0 +1,11 @@
+class Oystercard
+
+  def balance
+    balance = 0
+  end
+
+  def add_money(numb)
+    balance += numb
+  end
+  
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,13 @@
+require 'oystercard.rb'
+
 describe Oystercard do
+
+  it 'has a default balance' do
+    expect(subject.balance).to eq 0 
+  end
+
+  it 'adds money to card' do 
+    expect(subject).to respond_to(:add_money).with(1).argument
+  end
+
 end
